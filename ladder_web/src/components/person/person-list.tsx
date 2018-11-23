efa47606e551c5d62f54856de9622705b11bad9d
Damoon <damoon.rashidi@gmail.com>
2018-11-23 21:49:46 +0100

    started integrating firebase auth

--- ladder_web/src/components/person/person-list.tsx
+++ ladder_web/src/components/person/person-list.tsx
@@ -1,27 +1,27 @@
-import { Component, State } from '@stencil/core';
-import { css } from 'glamor';
-import { Person } from './person.interface';
+import { Component, State } from "@stencil/core";
+import { css } from "glamor";
+import { Person } from "./person.interface";
 
 const list = css({
-  width: `300px`,
+  width: `300px`
 }).toString();
 
 @Component({
-  tag: 'app-person-list',
+  tag: "app-person-list"
 })
 export class PersonList {
   @State() people: Person[] = [];
 
   async componentWillLoad() {
     this.people = await (await fetch(
-      'https://us-central1-ladder-41a39.cloudfunctions.net/people'
+      "https://us-central1-ladder-41a39.cloudfunctions.net/people"
     )).json();
   }
 
   toPerson() {
-    return this.people.sort((a,b) => b.points - a.points).map(person => (
-      <app-person name={person.name} points={person.points} />
-    ));
+    return this.people
+      .sort((a, b) => b.points - a.points)
+      .map(person => <app-person name={person.name} points={person.points} />);
   }
 
   render() {
