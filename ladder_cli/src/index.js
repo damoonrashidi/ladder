b4cfe681fe6e24a6c837913083855623fde7b4dc
Nicklas Nordenmark <nicklas.nordenmark@freespee.com>
2019-01-08 11:36:43 +0100

    Major design changes

--- ladder_cli/src/index.js
+++ ladder_cli/src/index.js
@@ -25,9 +25,25 @@ program
   .command('rankings')
   .description('Show current rankings of players')
   .action(async () => {
+    const name = settingsManager.get('name') || '';
+
     (await apiService.getRankings())
-      .map(person => `${person.points} ${person.name}`)
-      .forEach(person => console.log(person));
+      .map(person => {
+        if (person.name === name) {
+          return colors.blue(`${person.points} ${person.name}`);
+        }
+
+        return `${person.points} ${person.name}`;
+      })
+      .forEach((person, index) => {
+        const number = (index + 1).toString();
+        const prefix = (number.length === 1 ? ' ' : '') + number;
+        const suffix = index === 0 ? '[👑 of 🏓]' : '';
+        console.log(`${colors.white(prefix)}. ${person} ${suffix}`);
+        if (index === 4) {
+          console.log(colors.rainbow('--------------------------------'));
+        }
+      });
   });
 
 program
@@ -46,7 +62,10 @@ program
     const games = await apiService.getProfile(name);
     const data = [1500, ...games.map(game => game.rating)];
     console.log(asciichart.plot(data, { height: 10 }));
-    const wins = games.reduce((w, g) => (g.winner === name ? w + 1 : w), 0);
+    const wins = games.reduce(
+      (w, g) => (g.winner.toLowerCase() === name.toLowerCase() ? w + 1 : w),
+      0
+    );
     console.log(`${games.length} games played`);
     console.log(
       `${colors.green(wins)} wins, ${colors.red(games.length - wins)} losses`
