ffe502c5cf91f2d003c48568bf96c305b2a60e33
Magnus Crafoord <magnus.crafoord@freespee.com>
2019-02-01 13:37:14 +0100

    add socket service

---
+++ ladder_socket/index.ts
@@ -0,0 +1,33 @@
+import io from "socket.io";
+import http from "http";
+import { BehaviorSubject } from "rxjs";
+
+const responseSubject = new BehaviorSubject({
+  isBusy: false,
+  timestamp: new Date().toTimeString()
+});
+
+const httpServer = new http.Server();
+
+const server = io(httpServer);
+
+responseSubject.subscribe(response => {
+  server.sockets.emit("available", response);
+});
+
+server.sockets.on("connection", socket => {
+  socket.emit("available", "You are connected!");
+
+  socket.on("available", message => {
+    if (typeof message === "boolean") {
+      responseSubject.next({
+        isBusy: message === true,
+        timestamp: new Date().toTimeString()
+      });
+    }
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("listening on *:3000");
+});
