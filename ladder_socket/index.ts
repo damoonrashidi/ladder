9b3829cc8af44e138220fcff39cd61dae2306081
Magnus Crafoord <magnus.crafoord@freespee.com>
2019-02-01 17:51:30 +0100

    fix stuff

--- ladder_socket/index.ts
+++ ladder_socket/index.ts
@@ -1,24 +1,41 @@
+import express from "express";
 import io from "socket.io";
 import http from "http";
 import { BehaviorSubject } from "rxjs";
 
 const PORT = process.env.PORT || 8080;
 
+const app = express();
+const httpServer = new http.Server(app);
+const server = io(httpServer);
+
 const responseSubject = new BehaviorSubject({
   isBusy: false,
   timestamp: new Date().toTimeString()
 });
 
-const httpServer = new http.Server();
+interface Available {
+  isBusy: boolean;
+  timestamp: string;
+}
 
-const server = io(httpServer);
+let currentValue: Available;
 
 responseSubject.subscribe(response => {
   server.sockets.emit("available", response);
+  currentValue = response;
+});
+
+app.get("/", (_, res) => {
+  res.send(
+    currentValue.isBusy
+      ? `<h1>AVAILABLE</h1></br><h3>since: ${currentValue.timestamp}</h3>`
+      : `<h1>BUSY</h1>`
+  );
 });
 
 server.sockets.on("connection", socket => {
-  socket.emit("available", "You are connected!");
+  socket.emit("available", currentValue);
 
   socket.on("available", message => {
     if (typeof message === "boolean") {
